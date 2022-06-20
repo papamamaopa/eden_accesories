0a2dffbb92650b34deeefa3a45be9be1ee74829a
papamamaopa <steam.philb@gmail.com>
2022-06-20 21:29:14 +0200

    initial commit

---
+++ eden_weapon_accesories.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('yusuf', 'Luxus Skin', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('grip', 'Griff', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('flashlight', 'Lampe', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('silencieux', 'Schaldämpfer', 1, 0, 1);
